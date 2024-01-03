fc1becbe86ef5c7bf140217a9ddb4d2a6192d8a6
Melaka36 <melaka36pathiranagama@gmail.com>
2024-01-03 20:57:03 +0530

    poster loading done

--- movie-gold-v1/src/components/home/home.js
+++ movie-gold-v1/src/components/home/home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../hero/Hero'
 
-const Home = () => {
+const Home = ({movies}) => {
   return (
-    <div>home</div>
+    <Hero movies={movies}>  </Hero>
   )
 }
 
-export default Home
+export default Home 
