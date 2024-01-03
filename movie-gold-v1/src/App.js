eb49fe5486d6e45c534b5ac1052a7be84f01e9b6
Melaka36 <melaka36pathiranagama@gmail.com>
2024-01-03 16:01:45 +0530

    layout added

--- movie-gold-v1/src/App.js
+++ movie-gold-v1/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
-
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
 function App() {
 
   const[movies,setMovies] = useState([]);
@@ -20,7 +21,11 @@ function App() {
   
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={Layout}>
+          
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ movie-gold-v1/src/components/Layout.js
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+import React from 'react'
+
+const Layout = () => {
+  return (
+    // <main> is used to wrap the main content of each page
+    <main>
+        Layout
+    </main>
+  )
+}
+
+export default Layout
